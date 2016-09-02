10a4d0edf857ad6966fe5b6c3a2e6a43ed7747b3
Kozo Nishida <kozo.nishida@gmail.com>
2016-09-02 14:11:57 +0900

    replace dammy code with E-Cell simulation

--- Resources/downloader.py
+++ Resources/downloader.py
@@ -1,27 +1,50 @@
-import glob
-import re
+from ecell4 import *
+#from ecell4.egfrd import EGFRDWorld, EGFRDSimulator
+from ecell4.gillespie import GillespieWorld, GillespieSimulator
+import pandas as pd
 
-import http.client
+#w = EGFRDWorld()
+w = GillespieWorld()
 
-conn = http.client.HTTPConnection('rest.kegg.jp')
+sp1 = Species("A")
+sp2 = Species("B")
+sp3 = Species("C")
 
-for i in glob.glob("./*.txt"):
-    content = open(i).read()
-    kegg_org_codes = re.findall('[a-x]{3}', content)
-    for orgcode in kegg_org_codes:
-        conn.request("GET", "/list/pathway/" + orgcode)
-        response = conn.getresponse()
-        if response.status == 200:
-            for i in response.readlines():
-                pathid = i.decode('utf-8').split("\t")[0]
-                pathid = pathid.split(":")[1]
-                print("Downloading " + pathid)
-                newconn = http.client.HTTPConnection('rest.kegg.jp')
-                newconn.request("GET", "/get/" + pathid + "/kgml")
-                re = newconn.getresponse()
-                handle = open("..\\Output\\"+pathid+".xml", "w")
-                handle.write(re.read().decode('utf-8'))
-                handle.close()
-                print("finishded downloading for organism " + orgcode)
-        else:
-            print("Your organism code is not in KEGG")
+sp1.set_attribute("radius", "0.005")
+sp2.set_attribute("radius", "0.005")
+sp3.set_attribute("radius", "0.005")
+
+sp1.set_attribute("D", "1")
+sp2.set_attribute("D", "1")
+sp3.set_attribute("D", "1")
+
+# rr1 = ReactionRule()
+# rr1.add_reactant(sp1)
+# rr1.add_reactant(sp2)
+# rr1.add_product(sp3)
+# rr1.set_k(0.01)
+
+# rr2 = ReactionRule()
+# rr2.add_reactant(sp3)
+# rr2.add_product(sp1)
+# rr2.add_product(sp2)
+# rr2.set_k(0.3)
+
+m = NetworkModel()
+# m.add_reaction_rules([rr1, rr2])
+m.add_reaction_rule(create_binding_reaction_rule(sp1, sp2, sp3, 0.01))
+m.add_reaction_rule(create_unbinding_reaction_rule(sp3, sp1, sp2, 0.3))
+
+w.add_molecules(sp3, 60)
+
+obs = FixedIntervalNumberObserver(0.01, ["A", "B", "C"])
+
+#sim = EGFRDSimulator(m, w)
+sim = GillespieSimulator(m, w)
+sim.initialize()
+sim.run(10.0, obs)
+print(obs.data())
+a = pd.DataFrame(obs.data())
+a.to_csv("aaa.csv")
+#sim.step(0.001)
+ 
