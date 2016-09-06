2e107ad9c3c69611052aadcd5ddf694c3bb2c5c2
Kozo Nishida <knishida@riken.jp>
2016-09-07 00:56:10 +0900

    use argparse for the model csvs

--- Resources/ecell4-simulation.py
+++ Resources/ecell4-simulation.py
@@ -3,48 +3,51 @@ from ecell4 import *
 from ecell4.gillespie import GillespieWorld, GillespieSimulator
 import pandas as pd
 
-#w = EGFRDWorld()
-w = GillespieWorld()
+import argparse
+import csv
 
-sp1 = Species("A")
-sp2 = Species("B")
-sp3 = Species("C")
+def main(r, s):
+    m = NetworkModel()
+    w = GillespieWorld()
+    loglist = []
 
-sp1.set_attribute("radius", "0.005")
-sp2.set_attribute("radius", "0.005")
-sp3.set_attribute("radius", "0.005")
+    with open(s) as scsv:
+        sreader = csv.reader(scsv)
+        for row in sreader:
+            sp = Species(row[0], row[1], row[2])
+            m.add_species_attribute(sp)
+            # print(row[3])
+            if row[3] != '':
+                w.add_molecules(sp, int(row[3]))
+            loglist.append(row[0])
 
-sp1.set_attribute("D", "1")
-sp2.set_attribute("D", "1")
-sp3.set_attribute("D", "1")
+    with open(r) as rcsv:
+        rreader = csv.reader(rcsv)
+        for row in rreader:
+            if row[3] == '':
+                a = create_binding_reaction_rule(Species(row[0]), Species(row[1]), Species(row[2]), float(row[4]))
+                m.add_reaction_rule(a)
 
-# rr1 = ReactionRule()
-# rr1.add_reactant(sp1)
-# rr1.add_reactant(sp2)
-# rr1.add_product(sp3)
-# rr1.set_k(0.01)
+            elif row[1] == '':
+                a = create_unbinding_reaction_rule(Species(row[0]), Species(row[2]), Species(row[3]), float(row[4]))
+                m.add_reaction_rule(a)
 
-# rr2 = ReactionRule()
-# rr2.add_reactant(sp3)
-# rr2.add_product(sp1)
-# rr2.add_product(sp2)
-# rr2.set_k(0.3)
+    obs = FixedIntervalNumberObserver(0.01, loglist)
+    sim = GillespieSimulator(m, w)
+    sim.initialize()
+    sim.run(10.0, obs)
+    #print(obs.data())
+    a = pd.DataFrame(obs.data())
+    a.to_csv("aaa.csv")
 
-m = NetworkModel()
-# m.add_reaction_rules([rr1, rr2])
-m.add_reaction_rule(create_binding_reaction_rule(sp1, sp2, sp3, 0.01))
-m.add_reaction_rule(create_unbinding_reaction_rule(sp3, sp1, sp2, 0.3))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="read model csvs and simulate it")
+    parser.add_argument('-r',
+                        help='reaction csv')
 
-w.add_molecules(sp3, 60)
+    parser.add_argument('-s',
+                        help='species csv')
 
-obs = FixedIntervalNumberObserver(0.01, ["A", "B", "C"])
-
-#sim = EGFRDSimulator(m, w)
-sim = GillespieSimulator(m, w)
-sim.initialize()
-sim.run(10.0, obs)
-print(obs.data())
-a = pd.DataFrame(obs.data())
-a.to_csv("aaa.csv")
-#sim.step(0.001)
- 
+    args = parser.parse_args()
+    main(args.r, args.s)
