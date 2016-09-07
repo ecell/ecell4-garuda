c2e250bf555ed467f4f4268b1b77058ff6cbaf45
Kozo Nishida <knishida@riken.jp>
2016-09-07 17:41:01 +0900

    add stepsize and runtime argument

--- Resources/ecell4-simulation.py
+++ Resources/ecell4-simulation.py
@@ -6,12 +6,12 @@ import pandas as pd
 import argparse
 import csv
 
-def main(r, s):
+def main(rea, spe, step, runtime):
     m = NetworkModel()
     w = GillespieWorld()
     loglist = []
 
-    with open(s) as scsv:
+    with open(spe) as scsv:
         sreader = csv.reader(scsv)
         for row in sreader:
             sp = Species(row[0], row[1], row[2])
@@ -21,7 +21,7 @@ def main(r, s):
                 w.add_molecules(sp, int(row[3]))
             loglist.append(row[0])
 
-    with open(r) as rcsv:
+    with open(rea) as rcsv:
         rreader = csv.reader(rcsv)
         for row in rreader:
             if row[3] == '':
@@ -32,10 +32,10 @@ def main(r, s):
                 a = create_unbinding_reaction_rule(Species(row[0]), Species(row[2]), Species(row[3]), float(row[4]))
                 m.add_reaction_rule(a)
 
-    obs = FixedIntervalNumberObserver(0.01, loglist)
+    obs = FixedIntervalNumberObserver(float(step), loglist)
     sim = GillespieSimulator(m, w)
     sim.initialize()
-    sim.run(10.0, obs)
+    sim.run(float(runtime), obs)
     #print(obs.data())
     a = pd.DataFrame(obs.data())
     a.to_csv("aaa.csv")
@@ -43,11 +43,14 @@ def main(r, s):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="read model csvs and simulate it")
-    parser.add_argument('-r',
+    parser.add_argument('--reactions',
                         help='reaction csv')
-
-    parser.add_argument('-s',
+    parser.add_argument('--species',
                         help='species csv')
+    parser.add_argument('--stepsize',
+                        help='step size')
+    parser.add_argument('--runtime',
+                        help='run time')
 
     args = parser.parse_args()
-    main(args.r, args.s)
+    main(args.reactions, args.species, args.stepsize, args.runtime)
